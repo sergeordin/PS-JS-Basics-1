9c634861adf3a201cb833bd5ac03b3906560e5ab
Sergei Ordin <seryogaordin@gmail.com>
2023-02-22 16:32:30 +0300

    Добавил третий параметр и немного переписал функционал :)

--- app.js
+++ app.js
@@ -7,16 +7,34 @@
 возвращает число в новой валюте, если конвертация поддерживается или null, если нет. Ставки конвертации хранятся внутри функции
 */
 
-function convertCurrency(sum, currency = 'руб') {
+function convertCurrency (amount, from, to) {
 	const rubToUsd = 75;
+	const rubToEuro = 85;
 	const euroToUsd = 1.2;
-	if (currency === 'руб') {
-		return console.log(sum * rubToUsd);
-	} else if (currency === 'euro') {
-		return console.log(sum * euroToUsd);
+	const euroToRub = 1 / rubToEuro;
+	const usdToRub = 1 / rubToUsd;
+	const usdToEuro = 1 / euroToUsd;
+
+	if (from === 'RUB' && to === 'USD') {
+		return console.log(amount * rubToUsd);
+	} else if(from === 'RUB' && to === 'EUR') {
+		return console.log(amount * rubToEuro);
+	} else if(from === 'USD' && to === 'RUB') {
+		return console.log(amount * usdToRub);
+	} else if(from === 'USD' && to === 'EUR') {
+		return console.log(amount * usdToEuro);
+	} else if(from === 'EUR' && to === 'RUB') {
+		return console.log(amount * euroToRub);
+	} else if(from === 'EUR' && to === 'USD') {
+		return console.log(amount * euroToUsd);
 	} else {
 		return console.log(null);
 	}
 }
 
-convertCurrency(1000);
+
+let amountInput = prompt('Введите сумму');
+let fromInput = prompt('Введите исходную валюту (RUB, USD, EUR)');
+let toInput = prompt('Введите валюту, в которую нужно конвертировать (RUB, USD, EUR)');
+
+convertCurrency(amountInput, fromInput, toInput);
