ec7cc112be44c68c50cdd47201cca7632d67c415
Sergei Ordin <seryogaordin@gmail.com>
2023-02-22 16:45:32 +0300

    Переписал создание нового массива через обмен переменных

--- app.js
+++ app.js
@@ -12,12 +12,10 @@ function sortArr(arr) {
 	for (let i=0; i<arr.length; i++)
     {
 		for (let j = 0; j < arr.length - i - 1; j++) {
-			if (arr[j] > arr[j+1])
-                {
-                    temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp; 
-                } 
+			if (arr[j] > arr[j + 1])
+			{
+				[arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+			} 
 		}    
 	}
 	return arr
