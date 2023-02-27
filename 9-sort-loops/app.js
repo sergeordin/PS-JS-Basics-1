78838f5e5814a20b5afb09e95ee00094db54f17d
Sergei Ordin <seryogaordin@gmail.com>
2023-02-27 12:45:01 +0300

    Fix

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -8,13 +8,10 @@
 const arr = [1, 40, -5, 10, 0];
 
 function sortArr(arr) {
-    let temp = [];
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
             }
         }
     }
