d917bb466f56c72c9e7df08d3ba6021c19f32deb
Sergei Ordin <seryogaordin@gmail.com>
2023-02-28 16:35:15 +0300

    Решение через reduce

--- 12-luna/app.js
+++ 12-luna/app.js
@@ -4,31 +4,16 @@ function lunaAlgo(cardNumber) {
     const card = cardNumber.replaceAll('-', '');
     card.length < 16 ? console.log('Length error') : console.log('Length ok');
 
-    let sum = 0;
-    for (let i = card.length - 1; i >= 0; i--) {
-        let number = card[i];
-        if (i % 2 == 0) {
-            number *= 2;
-            if (number > 9) {
-                number -= 9;
+    const res = card.split('').reduce((acc, value, index) => {
+        if (index % 2 !== 0) {
+            value = value * 2;
+            if (value > 9) {
+                value = value - 9;
             }
         }
-        sum += number;
-        console.log(typeof number);
-    }
-    return sum % 10 == 0;
-
-    // let sum = card.reduce((acc, number, i) => {
-    //     console.log(acc, number);
-    //     if (i % 2 == 0) {
-    //         number *= 2;
-    //         if (number > 9) {
-    //             number -= 9;
-    //         }
-    //     }
-    //     acc += number;
-    // }, 0);
-    // console.log(sum)
+        return acc;
+    }, 0);
+    return res % 10 == 0;
 }
 
 console.log(lunaAlgo(cardNum));
