e69cf8c48fc083977cb7b86c3437d88d29349c37
Sergei Ordin <seryogaordin@gmail.com>
2023-03-02 18:57:05 +0300

    13 hw

--- 13-query/app.js
+++ 13-query/app.js
@@ -3,4 +3,22 @@ const query = {
     take: 10,
 };
 
-function searchStr(query) {}
+function searchStr(q) {
+    const values = [];
+    const keys = [];
+    let str = '';
+    for (key in q) {
+        keys.push(key);
+    }
+    for (val of Object.values(q)) {
+        values.push(val);
+    }
+    if (values.length == 1) {
+        str = `${keys[0]}=${values[0]}`;
+    } else if (values.length > 1) {
+        str = `${keys[0]}=${values[0]}&${keys[1]}=${values[1]}`;
+    }
+    return str;
+}
+
+console.log(searchStr(query));
